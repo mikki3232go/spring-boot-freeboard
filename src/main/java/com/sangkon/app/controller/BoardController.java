9a1b588fbefa45f8f9535b0021f95f3d884a379a
sigmadream <sigmadream@gmail.com>
2020-06-22 14:41:23 +0900

    form 입력

--- src/main/java/com/sangkon/app/controller/BoardController.java
+++ src/main/java/com/sangkon/app/controller/BoardController.java
@@ -1,12 +1,14 @@
 package com.sangkon.app.controller;
 
 
+import com.sangkon.app.domain.Board;
 import com.sangkon.app.service.BoardService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
@@ -22,4 +24,20 @@ public class BoardController {
         model.addAttribute("boardList", boardService.findBoardList(pageable));
         return "board/list";
     }
+
+    @GetMapping("/board")
+    public String getInputForm(Model model) {
+        model.addAttribute("boardForm", new Board());
+        return "board/form";
+    }
+
+    @PostMapping("/board")
+    public String setInputForm(Board boardForm) {
+        System.out.println(boardForm.getTitle());
+        System.out.println(boardForm.getSubTitle());
+        System.out.println(boardForm.getContent());
+        System.out.println(boardForm.getBoardType());
+        return "redirect:/board";
+    }
+
 }
--- src/main/java/com/sangkon/app/domain/Board.java
+++ src/main/java/com/sangkon/app/domain/Board.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @AllArgsConstructor
